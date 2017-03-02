41ace963f3f3a7fe630ba501b525f49bf268ef07
tbrams <torben@brams.dk>
2017-03-02 18:13:12 +0700

    Labels on tickmarks added

--- app/src/main/java/dk/brams/android/axistest1/CustomDrawableView.java
+++ app/src/main/java/dk/brams/android/axistest1/CustomDrawableView.java
@@ -1,6 +1,7 @@
 package dk.brams.android.axistest1;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.ShapeDrawable;
@@ -25,6 +26,8 @@ public class CustomDrawableView extends View {
     private int yMax=5;
     private int dX=25;
     private int dY=5;
+    private Paint mLinePaint;
+    private float mFontSize;
 
 
     public CustomDrawableView(Context context) {
@@ -42,6 +45,20 @@ public class CustomDrawableView extends View {
         mDrawable = new ShapeDrawable(new OvalShape());
         mDrawable.getPaint().setColor(0xff74AC23);
         mDrawable.setBounds(x, y, x + width, y + height);
+
+        /**
+         * This is the graphics style for the axis and tickmarks
+         */
+        mLinePaint = new Paint();
+        mLinePaint.setColor(0xff0000ee);
+        mLinePaint.setStrokeWidth(5);
+
+        Resources res = getResources();
+        mFontSize = res.getDimension(R.dimen.axis_text_size);
+        mLinePaint.setTextSize(mFontSize);
+//        mLinePaint.setTextAlign(Paint.Align.CENTER);
+
+
     }
 
 
@@ -57,14 +74,7 @@ public class CustomDrawableView extends View {
         mDrawable.draw(canvas);
 
 
-        /**
-         * This is the graphics style for the axis and tickmarks
-         */
-        Paint linePaint = new Paint();
-        linePaint.setColor(0xff0000ee);
-        linePaint.setStrokeWidth(5);
-
-        // Draw axis lines without scale on canvas - 10 is the margin we want to use here
+       // Draw axis lines without scale on canvas - 10 is the margin we want to use here
         // so the canvas coordinate limits are as follows:
         int xCanvasMin=10;
         int xCanvasMax=width-10-10;
@@ -79,19 +89,38 @@ public class CustomDrawableView extends View {
         double yAlpha = (double)(yCanvasMax-yCanvasMin)/(yMax-yMin);
         double y0 = yCanvasMax-yAlpha*yMax;
 
-        canvas.drawLine(xCanvasMin, (int)y0, xCanvasMax, (int)y0 ,linePaint);
+        canvas.drawLine(xCanvasMin, (int)y0, xCanvasMax, (int)y0 ,mLinePaint);
         for (int x=xMin; x<=xMax; x+=dX) {
             int xC=(int)(xAlpha*x+x0);
-            canvas.drawLine(xC, (int)(y0+20), xC, (int)y0 ,linePaint);
+            canvas.drawLine(xC, (int)(y0+20), xC, (int)y0 ,mLinePaint);
+            if (x!=0)
+               drawTextCentered(String.format("%d",x),xC, (int) (y0+20+mFontSize*.667), mLinePaint, canvas);
         }
 
         // Y-Axis
-        canvas.drawLine((int)x0, yCanvasMin, (int)x0, yCanvasMax,linePaint);
+        canvas.drawLine((int)x0, yCanvasMin, (int)x0, yCanvasMax,mLinePaint);
         for (int y=yMin; y<=yMax; y+=dY) {
             int yC=(int)(yAlpha*y+y0);
-            canvas.drawLine((int)(x0-20), yC, (int)x0, yC ,linePaint);
+            canvas.drawLine((int)(x0-20), yC, (int)x0, yC ,mLinePaint);
+            if (y!=0)
+                drawTextRightJustified(String.format("%d",y), (int)(x0-20-mFontSize/2), yC, mLinePaint, canvas);
         }
 
+    }
+
+    private void drawTextCentered(String text, int x, int y, Paint paint, Canvas canvas) {
+        int xPos = x - (int)(paint.measureText(text)/2);
+        int yPos = (int) (y - ((paint.descent() + paint.ascent()) / 2)) ;
+
+        canvas.drawText(text, xPos, yPos, paint);
+    }
+
+
+    private void drawTextRightJustified(String text, int x, int y, Paint paint, Canvas canvas) {
+        int xPos = x - (int)(paint.measureText(text));
+        int yPos = (int) (y - ((paint.descent() + paint.ascent()) / 2)) ;
+
+        canvas.drawText(text, xPos, yPos, paint);
     }
 
 }
