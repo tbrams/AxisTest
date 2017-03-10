d9266c3ba8be468a913920c509738ced2df9a52f
tbrams <torben@brams.dk>
2017-03-10 19:38:56 +0100

    Rewritten setup and scaling logic so that we can easier extend this

--- app/src/main/java/dk/brams/android/axistest1/CustomDrawableView.java
+++ app/src/main/java/dk/brams/android/axistest1/CustomDrawableView.java
@@ -20,12 +20,25 @@ public class CustomDrawableView extends View {
      * The settings below are the limits on the coordinate system and the step size on each axis.
      */
 
-    private int xMin=-50;
-    private int xMax=25;
-    private int yMin=-30;
-    private int yMax=5;
-    private int dX=25;
-    private int dY=5;
+    private int mCanvasMinX;
+    private int mCanvasMaxX;
+    private int mCanvasMinY;
+    private int mCanvasMaxY;
+
+    private int mXmin;
+    private int mXmax;
+    private int mYmin;
+    private int mYmax;
+    private int mDx;
+    private int mDy;
+
+    private double mAlphaX;
+    private double mX0;
+    private double mAlphaY;
+    private double mY0;
+
+
+
     private Paint mLinePaint;
     private float mFontSize;
 
@@ -33,8 +46,12 @@ public class CustomDrawableView extends View {
     public CustomDrawableView(Context context) {
         super(context);
 
+
         /**
-         * This is just to have something on the canvas - here is an ellipse...
+         * This is a drawable with an ellipse...
+         * Just to have something to plot besides axis system.
+         *
+         * Notice scale is not related to axis values here
          */
 
         int x = 10;
@@ -46,9 +63,11 @@ public class CustomDrawableView extends View {
         mDrawable.getPaint().setColor(0xff74AC23);
         mDrawable.setBounds(x, y, x + width, y + height);
 
-        /**
-         * This is the graphics style for the axis and tickmarks
-         */
+
+        // Initialize axis values
+        setAxisValues(-50, 25, 25, -30, 5, 5);
+
+        // Initialize the graphics style for the axis and tick marks
         mLinePaint = new Paint();
         mLinePaint.setColor(0xff0000ee);
         mLinePaint.setStrokeWidth(5);
@@ -64,49 +83,113 @@ public class CustomDrawableView extends View {
 
     protected void onDraw(Canvas canvas) {
 
-        /**
-         * Attempt to scale the coordinate system to aling with the available canvas size
-         */
+        // Plot whatever is defined  above in the drawable as well
+        mDrawable.draw(canvas);
+
+        // The overlay the axis system
+        initializePlotArea(canvas);
+        drawAxisSystem(canvas);
+    }
+
+
+
+    /**
+     * Initialize the scale of the graph in both dimensions.
+     *
+     * @param xMin lowest x value on chart
+     * @param xMax  highest x value on chart
+     * @param dx    distance between x tickmarks
+     * @param yMin  lowest y value on chart
+     * @param yMax  highest y value on chart
+     * @param dy    distance between y tickmarks
+     */
+    private void setAxisValues(int xMin, int xMax, int dx, int yMin, int yMax, int dy){
+        mXmin = xMin;
+        mXmax = xMax;
+        mDx = dx;
+        mYmin = yMin;
+        mYmax = yMax;
+        mDy = dy;
+    }
+
+
+    /**
+     * Will initialize the canvas parameters based on canvas size.
+     *
+     * @param canvas
+     */
+    private void initializePlotArea(Canvas canvas) {
 
         int height= canvas.getHeight();
         int width = canvas.getWidth();
 
-        mDrawable.draw(canvas);
+        // Define the canvas coordinate limits so that we leave a margin around the graph
+        int margin = 10;
+
+        mCanvasMinX = margin;
+        mCanvasMaxX = width-margin-margin;
+
+        mCanvasMinY = height-margin-margin;
+        mCanvasMaxY = margin;
 
 
-       // Draw axis lines without scale on canvas - 10 is the margin we want to use here
-        // so the canvas coordinate limits are as follows:
-        int xCanvasMin=10;
-        int xCanvasMax=width-10-10;
+        // Define parameters for converting values to canvas equivalents
+        mAlphaX = (mCanvasMaxX-mCanvasMinX)/(mXmax - mXmin);
+        mX0 = mCanvasMaxX-mAlphaX* mXmax;
 
-        int yCanvasMin = height-10-10;
-        int yCanvasMax = 10;
+        mAlphaY = (double)(mCanvasMaxY-mCanvasMinY)/(mYmax - mYmin);
+        mY0 = mCanvasMaxY-mAlphaY* mYmax;
+
+
+    }
+
+
+    private void drawAxisSystem(Canvas canvas) {
 
         // X-Axis
-        double xAlpha = (xCanvasMax-xCanvasMin)/(xMax-xMin);
-        double x0 = xCanvasMax-xAlpha*xMax;
-
-        double yAlpha = (double)(yCanvasMax-yCanvasMin)/(yMax-yMin);
-        double y0 = yCanvasMax-yAlpha*yMax;
-
-        canvas.drawLine(xCanvasMin, (int)y0, xCanvasMax, (int)y0 ,mLinePaint);
-        for (int x=xMin; x<=xMax; x+=dX) {
-            int xC=(int)(xAlpha*x+x0);
-            canvas.drawLine(xC, (int)(y0+20), xC, (int)y0 ,mLinePaint);
+        canvas.drawLine(mCanvasMinX, (int)mY0, mCanvasMaxX, (int)mY0 ,mLinePaint);
+        for (int x = mXmin; x<= mXmax; x+= mDx) {
+            int xC= valueToCanvasX(x);
+            canvas.drawLine(xC, (int)(mY0+20), xC, (int)mY0 ,mLinePaint);
             if (x!=0)
-               drawTextCentered(String.format("%d",x),xC, (int) (y0+20+mFontSize*.667), mLinePaint, canvas);
+               drawTextCentered(String.format("%d",x),xC, (int) (mY0+20+mFontSize*.667), mLinePaint, canvas);
         }
 
         // Y-Axis
-        canvas.drawLine((int)x0, yCanvasMin, (int)x0, yCanvasMax,mLinePaint);
-        for (int y=yMin; y<=yMax; y+=dY) {
-            int yC=(int)(yAlpha*y+y0);
-            canvas.drawLine((int)(x0-20), yC, (int)x0, yC ,mLinePaint);
+        canvas.drawLine((int)mX0, mCanvasMinY, (int)mX0, mCanvasMaxY,mLinePaint);
+        for (int y = mYmin; y<= mYmax; y+= mDy) {
+            int yC= valueToCanvasY(y);
+            canvas.drawLine((int)(mX0-20), yC, (int)mX0, yC ,mLinePaint);
             if (y!=0)
-                drawTextRightJustified(String.format("%d",y), (int)(x0-20-mFontSize/2), yC, mLinePaint, canvas);
+                drawTextRightJustified(String.format("%d",y), (int)(mX0-20-mFontSize/2), yC, mLinePaint, canvas);
         }
-
     }
+
+
+    /**
+     * Map real Y value to canvas value.
+     *
+     * @param y value from setup
+     * @return equivalent canvas y value
+     */
+    private int valueToCanvasY(int y) {
+        return (int)(mAlphaY*y+mY0);
+    }
+
+
+
+    /**
+     * Map real X value to canvas value.
+     *
+     * @param x value from setup
+     * @return equivalent canvas x value
+     */
+    private int valueToCanvasX(int x) {
+
+        return (int)(mAlphaX*x+mX0);
+    }
+
+
 
     private void drawTextCentered(String text, int x, int y, Paint paint, Canvas canvas) {
         int xPos = x - (int)(paint.measureText(text)/2);
